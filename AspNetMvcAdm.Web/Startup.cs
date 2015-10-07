711218b75def64c7af09bb08dbff23c5077fd0e0
mkvster <mkvster@gmail.com>
2015-10-07 11:40:18 -0400

    Initial checkin

---
+++ AspNetMvcAdm.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvcAdm.Web.Startup))]
+namespace AspNetMvcAdm.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
